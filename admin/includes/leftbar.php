4a1a08daa01a4f4131b7aedd0fa3e33fa7d2dc9c
wafihamdani <wafihamdani2448@gmail.com>
2019-06-28 21:00:10 +0700

    Praktikum

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,31 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i>Type</a>
+<ul>
+<li><a href="create-type.php"> Create Type</a></li>
+<li><a href="manage-type.php"> Manage Type</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i>Stuff</a>
+					<ul>
+						<li><a href="post-stuff.php"> Post a Stuff</a></li>
+						<li><a href="manage-stuff.php"> Manage Stuff</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop""></i> Manage Conatct Us Query</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li>
+
+			</ul>
+		</nav>
